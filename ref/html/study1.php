83ad98c3ca82c51727d13baa758140f13d35c163
LEEsanghyo <tunyou2005@gmail.com>
2017-09-27 21:33:33 +0900

    2012722037 이상효

---
+++ ref/html/study1.php
@@ -0,0 +1,213 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+  <head lang="ko">
+    <meta charset="utf-8">
+    <meta nam="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SIGN UP</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <style>
+    #contents_img {
+      max-width:300px;
+      height:auto;
+      margin-right:10px;
+    }
+    @font-face {
+      font-family:"Wet_Dream";
+      src:url("./font/Wet_Dream.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    @font-face {
+      font-family:"SangSangFlowerRoad";
+      src:url("./font/SangSangFlowerRoad.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    @font-face {
+      font-family:"HoonSlimskinnyL";
+      src:url("./font/HoonSlimskinnyL.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    @font-face {
+      font-family:"baramB";
+      src:url("./font/Baramddara/baramB.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    @font-face {
+      font-family:"unlearne";
+      src:url("./font/unlearned/unlearne.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    @font-face {
+      font-family:"HanS";
+      src:url("./HanS.ttf") format('truetype');
+      font-style:normal;
+      font-weight:normal;
+    }
+    .first_comment1 {
+      font-family:HanS, sans-serif;
+      font-size:20pt;
+    }
+    .second_comment1 {
+      font-family:HanS, sans-serif;
+      font-size:60pt;
+    }
+    .second_comment2 {
+      font-family:HanS, sans-serif;
+      font-size:25pt;
+    }
+    .third_comment {
+      font-family:HanS, sans-serif;
+      font-size:30pt;
+      margin-top:1%;
+      margin-bottom:5%;
+    }
+    .third_comment2 {
+      font-family:HanS, sans-serif;
+      font-size:30pt;
+      margin-left: 15%;
+    }
+    .third_comment2 {
+      font-family:HanS, sans-serif;
+      font-size:30pt;
+      margin-left: 16%;
+      margin-top:2%;
+    }
+    .text_on_image3 {
+      background: rgba(250,128,114,0.7);
+      width:auto;
+    }
+    #logo {
+      max-width:100px;
+      height:auto;
+    }
+    .wrap {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -86px;
+        margin-left: -89px;
+        text-align: center;
+    }
+    a.button {
+      -webkit-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
+      -moz-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
+      -ms-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
+      -o-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
+      transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
+      display: block;
+      margin: 20px auto;
+      max-width: 260px;
+      text-decoration: none;
+      border-radius: 4px;
+      padding: 20px 20px;
+      color: rgba(30, 22, 54, 0.6);
+      box-shadow: rgba(30, 22, 54, 0.4) 0 0px 0px 2px inset;
+      font-size:13.5pt;
+    }
+
+    a.button:hover {
+        color: rgba(255, 255, 255, 0.85);
+        box-shadow: rgba(30, 22, 54, 0.7) 0 0px 0px 40px inset;
+    }
+    a.button2 {
+      -webkit-transition: all 200ms cubic-bezier(0.200, 0.500, 0.350, 1.360);
+      -moz-transition: all 200ms cubic-bezier(0.2000, 0.500, 0.350, 1.360);
+      -ms-transition: all 200ms cubic-bezier(0.200, 0.500, 0.350, 1.360);
+      -o-transition: all 200ms cubic-bezier(0.200, 0.500, 0.350, 1.360);
+      transition: all 200ms cubic-bezier(0.200, 0.500, 0.350, 1.360);
+      max-width: 100px;
+      padding:5px;
+      padding-left: 20px;
+      padding-right: 20px;
+      text-decoration: none;
+      border-radius: 4px;
+      color: rgba(60, 30, 80, 0.6);
+      box-shadow: rgba(50, 10, 70, 0.4) 0 0px 0px 2px inset;
+    }
+    a.button2:hover {
+      color: rgba(255, 255, 255, 0.85);
+      box-shadow: rgba(30, 22, 54, 0.7) 0 80px 0px 2px inset;
+    }
+    .navbar {
+      background: rgba(244,244,244,0.7);
+    }
+    .second_rows {
+      background: url(images/beach3.jpg) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+    .tb{
+      margin-left: 7%;
+    }
+    img {
+      max-width: 300px;
+      max-height: 150px;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="row">
+      <table class="tb">
+      <td class="third_comment2">2012722037 압도적인 이상효</td>
+      <td class="im"> <img src="./jjj.jpg" /> </td>
+      </table>
+    </div>
+    <br><br><br><hr/>
+    <hr style="border:1.5px gray solid"/>
+    <tr>
+    <td width='100%' height='200' align='left' valign='top' bgcolor='#FFFFFF'>
+    <div class="row">
+    <form class="form-horizontal" role="form" autocomplete="off" action='./test8.php' name='test' method='post'>
+    <div class="form-group" id="divId">
+        <label for="inputId" class="second_comment2 col-sm-2 control-label">빨리입력을해라</label>
+        <div class="col-sm-5" style="margin-top:1%">
+            <input type="text" autocomplete="off" name="user_id" class="form-control onlyAlphabetAndNumber" id="id" data-rule-required="true" placeholder="뭔가를 입력하라" maxlength="30">
+            <br>
+        </div>
+        <div class="button_wrap col-sm-offset-5" style="margin-top:20px">
+            <a class="button2">확~인</a>
+        </div>
+    </div>
+    </form>
+    </div>
+    </td>
+    <tr>
+    <td width='100%' height='100%' align='left' valign='top' bgcolor='#FFFFFF'>&nbsp;</td>
+    <hr size='0.1' />
+    <?php
+    include("file://C:/Bitnami/wamp/apache2/htdocs/jungwon/test4.php");
+    $connect_table = db_conn();
+    ?>
+   </tr>
+ </table>
+ </TD>
+ </TR>
+ </TABLE>
+ <div class="row">
+   <div class = "button_wrap">
+     <li><a href="./index.php" class="button"> 궁금한게 많은 기선이▶◀</a></li>
+   </div>
+ </div>
+ <hr/>
+ <!-- 푸터 들어가는 부분 -->
+
+ <div>
+     <p class="text-center">
+         <small> @Copyrighy by </small><br>
+         <small><strong> 지웅아 너 왜그러냐 진짜</strong></small><br>
+     </p>
+ </div>
+  </body>
+</html>
